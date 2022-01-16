e117d443d461835315e68667dc4b1aba98bb4da1
nack1400 <nack1400@naver.com>
2022-01-16 21:18:17 +0900

    basic webdev

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,47 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
   @GetMapping("hello")
-  public String hello(Model model){
-    model.addAttribute("data","hello!!");
+  public String hello(Model model) {
+    model.addAttribute("data", "hello!!");
     return "hello";
+  }
+
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam("name") String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam("name") String name) {
+    return "hello " + name;
+  }
+
+  //태그없이 그대로 옮겨주므로 json 같은 데이터를 전달할때 많이 씀
+  @GetMapping("hello-api")
+  @ResponseBody //이걸 사용하면 뷰리졸버를 사용하지 않음, 그대로 넘김, 이때 객체라면 json 방식으로 넘김
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
+
+  static class Hello {
+    private String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
